e5e95e10cb24d55ec32e06373059326e119279bc
Damian <Damian.berns@student.aventus.nl>
2019-08-06 21:00:43 +0200

    part of navigationbar fix

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,39 +1,45 @@
 <html>
-	<head>
-		<title>ciBlog</title>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
-    <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
-	</head>
-	<body>
-	<nav class="navbar navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
-        </div>
-        <div id="navbar">
-          <ul class="nav navbar-nav">
-            <li><a href="<?php echo base_url(); ?>">Home</a></li>
-            <li><a href="<?php echo base_url(); ?>about">About</a></li>
-             <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
-             <li><a href="<?php echo base_url(); ?>categories">Categories</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-          <?php if(!$this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
-            <li><a href="<?php echo base_url(); ?>users/register">Register</a></li>
-          <?php endif; ?>
-          <?php if($this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
-            <li><a href="<?php echo base_url(); ?>categories/create">Create Category</a></li>
-            <li><a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
-          <?php endif; ?>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<head>
+	<title>ciBlog</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+	<script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
-    <div class="container">
+	<!-- Brand/logo -->
+	<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+
+	<!-- Links -->
+		<ul class="nav navbar-nav navbar-left">
+		<li class="nav-item"><a class="nav-link"href="<?php echo base_url(); ?>">Home</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a></li>
+	</ul>
+
+	<ul class="nav navbar-nav navbar-right">
+		<?php if (!$this->session->userdata('logged_in')) : ?>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Login</a></li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a>
+			</li>
+		<?php endif; ?>
+		<?php if ($this->session->userdata('logged_in')) : ?>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
+			</li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories/create">Create
+					Category</a></li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout</a></li>
+		<?php endif; ?>
+	</ul>
+</nav>
+<br>
+<br>
+
+<div class="container">
       <!-- Flash messages -->
       <?php if($this->session->flashdata('user_registered')): ?>
         <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_registered').'</p>'; ?>
