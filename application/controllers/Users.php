a68c94020bb5f2ddbb953c9948c0cc9674deda30
Damian <Damian@berns.com>
2019-08-07 10:27:40 +0200

    Add files via upload
    pagination and some function updates to improve application

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,19 +1,22 @@
 <?php
 	class Users extends CI_Controller{
-		// Register user
+		// register user
 		public function register(){
 			$data['title'] = 'Sign Up';
 
 			$this->form_validation->set_rules('name', 'Name', 'required');
-			$this->form_validation->set_rules('username', 'Username', 'required|callback_check_username_exists');
-			$this->form_validation->set_rules('email', 'Email', 'required|callback_check_email_exists');
+
+			$this->form_validation->set_rules('username', 'Username', 'trim|min_length[3]|required|max_length[15]|is_unique[users.username]', array('is_unique' => 'This username already exists in our records.'));
+			$this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]', array('is_unique' => 'This email already exists in our records.'));
 			$this->form_validation->set_rules('password', 'Password', 'required');
 			$this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
 
-			if($this->form_validation->run() === FALSE){
+			if ( $this->form_validation->run() === FALSE){
+
 				$this->load->view('templates/header');
 				$this->load->view('users/register', $data);
 				$this->load->view('templates/footer');
+
 			} else {
 				// Encrypt password
 				$enc_password = md5($this->input->post('password'));
@@ -21,27 +24,33 @@
 				$this->user_model->register($enc_password);
 
 				// Set message
-				$this->session->set_flashdata('user_registered', 'You are now registered and can log in');
+				$this->session->set_flashdata('user_registered', 'You are now registered en can log in');
+
 
 				redirect('posts');
 			}
 		}
 
-		// Log in user
+// login user
+
 		public function login(){
-			$data['title'] = 'Sign In';
+			$data['title'] = 'sign in';
+
+
 
 			$this->form_validation->set_rules('username', 'Username', 'required');
 			$this->form_validation->set_rules('password', 'Password', 'required');
 
-			if($this->form_validation->run() === FALSE){
+			if ( $this->form_validation->run() === FALSE){
+
 				$this->load->view('templates/header');
 				$this->load->view('users/login', $data);
 				$this->load->view('templates/footer');
+
 			} else {
-				
 				// Get username
-				$username = $this->input->post('username');
+
+				$username  = $this->input->post('username');
 				// Get and encrypt the password
 				$password = md5($this->input->post('password'));
 
@@ -51,53 +60,60 @@
 				if($user_id){
 					// Create session
 					$user_data = array(
-						'user_id' => $user_id,
-						'username' => $username,
-						'logged_in' => true
+					'user_id' => $user_id,
+					'username' => $username,
+					'logged_in' => true
 					);
-
 					$this->session->set_userdata($user_data);
 
 					// Set message
 					$this->session->set_flashdata('user_loggedin', 'You are now logged in');
 
+
 					redirect('posts');
+
 				} else {
 					// Set message
 					$this->session->set_flashdata('login_failed', 'Login is invalid');
 
+
 					redirect('users/login');
-				}		
+				}
 			}
 		}
-
 		// Log user out
-		public function logout(){
-			// Unset user data
-			$this->session->unset_userdata('logged_in');
-			$this->session->unset_userdata('user_id');
-			$this->session->unset_userdata('username');
+	public function logout(){
+// Unset user data
+		$this->session->unset_userdata('logged_in');
+		$this->session->unset_userdata('user_id');
+		$this->session->unset_userdata('username');
 
-			// Set message
-			$this->session->set_flashdata('user_loggedout', 'You are now logged out');
-
-			redirect('users/login');
+		//Set message
+		$this->session->set_flashdata('user_loggedout', 'You are now logged out');
+		redirect('users/login');
 		}
 
 		// Check if username exists
-		public function check_username_exists($username){
-			$this->form_validation->set_message('check_username_exists', 'That username is taken. Please choose a different one');
-			if($this->user_model->check_username_exists($username)){
+
+		public function check_username_exists($username)
+		{
+			$this->form_validation->set_message('
+			check_username_exists', 'That username is taken.
+			please choose a different one');
+			if ($this->user_model->check_username_exists($username)){
 				return true;
 			} else {
 				return false;
 			}
 		}
-
 		// Check if email exists
-		public function check_email_exists($email){
-			$this->form_validation->set_message('check_email_exists', 'That email is taken. Please choose a different one');
-			if($this->user_model->check_email_exists($email)){
+
+		public function check_email_exists($email)
+		{
+			$this->form_validation->set_message('
+			check_email_exists', 'That email is taken.
+			please choose a different one');
+			if ($this->user_model->check_email_exists($email)){
 				return true;
 			} else {
 				return false;
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,78 +1,90 @@
+<!--Part one: https://www.youtube.com/watch?v=I752ofYu7ag -->
+<!--Part two: https://www.youtube.com/watch?v=jOKTjE6Q5QQ -->
+<!--Part three: https://www.youtube.com/watch?v=ay_w82osHpM -->
+<!--Part four https://www.youtube.com/watch?v=01vd1kF9vZg  -->
+<!-- Part five https://www.youtube.com/watch?v=bXAT2uO3sw8 -->
+<!-- Part six https://www.youtube.com/watch?v=qS0a4nKQ6h8 -->
+<!-- Part seven  https://www.youtube.com/watch?v=Ah65hBRffwc -->
+<!-- Part eight  https://www.youtube.com/watch?v=WoQTjJepDWM -->
+
 <html>
 <head>
 	<title>ciBlog</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
 	<script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-
-	<!-- Brand/logo -->
-	<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
-
-	<!-- Links -->
-		<ul class="nav navbar-nav navbar-left">
-		<li class="nav-item"><a class="nav-link"href="<?php echo base_url(); ?>">Home</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a></li>
-	</ul>
-
-	<ul class="nav navbar-nav navbar-right">
-		<?php if (!$this->session->userdata('logged_in')) : ?>
-			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Login</a></li>
-			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a>
-			</li>
-		<?php endif; ?>
-		<?php if ($this->session->userdata('logged_in')) : ?>
-			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
-			</li>
-			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories/create">Create
-					Category</a></li>
-			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout</a></li>
-		<?php endif; ?>
-	</ul>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active"><a class="nav-link" href="<?php echo base_url(); ?>">Home</a></li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a></li>
+		</ul>
+	</div>
+	<div class="mx-auto order-0">
+		<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+	</div>
+	<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+		<ul class="navbar-nav ml-auto">
+			<?php if (!$this->session->userdata('logged_in')) : ?>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Login</a></li>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a>
+				</li>
+			<?php endif; ?>
+			<?php if ($this->session->userdata('logged_in')) : ?>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
+				</li>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories/create">Create
+						Category</a></li>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout</a></li>
+			<?php endif; ?>
+		</ul>
 </nav>
+
 <br>
 <br>
+
 
 <div class="container">
-      <!-- Flash messages -->
-      <?php if($this->session->flashdata('user_registered')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_registered').'</p>'; ?>
-      <?php endif; ?>
+<!--	flash messages -->
+	<?php if ($this->session->flashdata('user_registered')): ?>
+	<?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_registered').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('post_created')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_created').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('post_created')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_created').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('post_updated')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_updated').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('post_updated')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_updated').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('category_created')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_created').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('category_created')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_created').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('post_deleted')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_deleted').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('post_deleted')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_deleted').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('login_failed')): ?>
-        <?php echo '<p class="alert alert-danger">'.$this->session->flashdata('login_failed').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('login_failed')): ?>
+		<?php echo '<p class="alert alert-danger">'.$this->session->flashdata('login_failed').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('user_loggedin')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedin').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('login_loggedin')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('login_loggedin').'</p>'; ?>
+	<?php endif; ?>
 
-       <?php if($this->session->flashdata('user_loggedout')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedout').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('login_loggedout')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('login_loggedout').'</p>'; ?>
+	<?php endif; ?>
 
-      <?php if($this->session->flashdata('category_deleted')): ?>
-        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_deleted').'</p>'; ?>
-      <?php endif; ?>
+	<?php if ($this->session->flashdata('category_deleted')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_deleted').'</p>'; ?>
+	<?php endif; ?>
